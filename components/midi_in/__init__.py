4afc3b626330b99df5416ba1b0cad2e20d1fce46
Bernadette <git@berni.kr>
2026-02-19 22:42:45 +0100

    fix binary sensor config

--- components/midi_in/__init__.py
+++ components/midi_in/__init__.py
@@ -136,12 +136,12 @@ CONFIG_SCHEMA = cv.All(
                     ),
                 }
             ),
-            cv.Optional(CONF_CONNECTED): binary_sensor.BINARY_SENSOR_SCHEMA.extend(
+            cv.Optional(CONF_CONNECTED): binary_sensor._BINARY_SENSOR_SCHEMA.extend(
                 {
                     cv.GenerateID(): cv.declare_id(binary_sensor.BinarySensor),
                 }
             ),
-            cv.Optional(CONF_PLAYBACK): binary_sensor.BINARY_SENSOR_SCHEMA.extend(
+            cv.Optional(CONF_PLAYBACK): binary_sensor._BINARY_SENSOR_SCHEMA.extend(
                 {
                     cv.GenerateID(): cv.declare_id(binary_sensor.BinarySensor),
                 }
